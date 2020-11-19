3cdbd08df4c9b7c133d90e1f762f781d24e8576a
Sebastian Schloßer <sebastian@schweizer.tel>
2020-11-19 11:24:01 +0100

    introduce actual jOOQ code

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,7 +1,13 @@
 package demo;
 
+import org.jooq.Allow;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
 public class App {
+    @Allow(SQLDialect.H2)
+    // @Allow.PlainSQL
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DSL.condition("true");
     }
 }
